e407032a5eeca65fb19777c6cf555e13939c0d0f
Maor Maharizi <maharizi96@gmail.com>
2023-01-24 16:20:50 +0200

    add switch case

--- Item.cs
+++ Item.cs
@@ -8,9 +8,43 @@ namespace amazonEx1
 {
     internal class Item
     {
-        public string url;
-        public string titel;
-        public string price;
+        private string url;
+        private string titel;
+        private string price;
 
+        public string Url
+        {
+            set
+            {
+                this.url = value;
+            }
+            get
+            {
+                return this.url;
+            }
+        }
+        public string Titel 
+        {
+            set
+            {
+                this.titel = value;
+            }
+            get
+            {
+                return this.titel;
+            }
+        }
+
+        public string Price
+        {
+            set
+            {
+                this.price = value;
+            }
+            get
+            {
+                return this.price;
+            }
+        }
     }
 }
--- Results.cs
+++ Results.cs
@@ -17,22 +17,23 @@ namespace amazonEx1
 
         public void getResultBy(Dictionary<string, string> filters)
         {
-            string xpath = "//span[@class='a-price-whole']";
+            string xpath = "//span[@class='a-price-whole'";
             foreach(var filter in filters)
             {
                 switch (filter.Key)
                 {
                     case "Price_Lower_Then":
-                        xpath += "" + filter.Value;
+                        xpath += "and descendant::span[@class = 'a-price-whole' and text() < " + filter.Value;
                         break;
                     case "Price_Hiegher_OR_Equal_Then":
-                        xpath += "" + filter.Value;
+                        xpath += "and text() >= " + filter.Value;
                         break;
                     case "Free_Shipping":
-                        xpath += "" + filter.Value;
+                        xpath += "and descendant::span[contains(text(), 'FREE'" + filter.Value;
                         break;
                 }
             }
+            xpath += "]]";
         }
     }
 }
